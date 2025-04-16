da5ab0b0957e655ee4911b53c828fbee154ee26b
Otávio Ximarelli <146153429+OtavioXimarelli@users.noreply.github.com>
2025-04-16 14:06:38 +0000

    fix: remove unused imports and add nutritional fields to FoodItem model

--- src/main/java/com/otavio/aifoodapp/model/FoodItem.java
+++ src/main/java/com/otavio/aifoodapp/model/FoodItem.java
@@ -3,10 +3,7 @@ package com.otavio.aifoodapp.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.context.annotation.Bean;
-
 import java.time.LocalDate;
 
 @Entity
@@ -23,6 +20,16 @@ public class FoodItem {
     private String type;
     private Integer quantity;
     private LocalDate expiration;
+    private double calories;
+    private double protein;
+    private double fat;
+    private double carbohydrates;
+    private double fiber;
+    private double sugar;
+    private double sodium;
+
+
+
 
 
     public Long getId() {
@@ -63,5 +70,48 @@ public class FoodItem {
 
     public void setExpiration(LocalDate expiration) {
         this.expiration = expiration;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
+    public double getProtein() {
+        return protein;
+    }
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+    public double getFat() {
+        return fat;
+    }
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+    public double getCarbohydrates() {
+        return carbohydrates;
+    }
+    public void setCarbohydrates(double carbohydrates) {
+        this.carbohydrates = carbohydrates;
+    }
+    public double getFiber() {
+        return fiber;
+    }
+    public void setFiber(double fiber) {
+        this.fiber = fiber;
+    }
+    public double getSugar() {
+        return sugar;
+    }
+    public void setSugar(double sugar) {
+        this.sugar = sugar;
+    }
+    public double getSodium() {
+        return sodium;
+    }
+    public void setSodium(double sodium) {
+        this.sodium = sodium;
     }
 }
