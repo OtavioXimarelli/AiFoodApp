3855f60480a53b40e9e1d14d3b19a763a0d9b4a8
OtavioXimarelli <ximarellio@gmail.com>
2025-04-18 02:06:22 -0300

    feat: add nutrition fields and food group to tb_food_item table, and create tb_food_items_tag table

---
+++ src/main/resources/db/migration/V2__Add _nutrition_fields_and_food_group.sql
@@ -0,0 +1,19 @@
+ALTER TABLE tb_food_item
+        ADD COLUMN calories DOUBLE PRECISION,
+        ADD COLUMN protein DOUBLE PRECISION,
+        ADD COLUMN fat DOUBLE PRECISION,
+        ADD COLUMN carbohydrates DOUBLE PRECISION,
+        ADD COLUMN fiber DOUBLE PRECISION,
+        ADD COLUMN sugar DOUBLE PRECISION,
+        ADD COLUMN sodium DOUBLE PRECISION,
+        ADD COLUMN food_group VARCHAR(50);
+
+ALTER TABLE tb_food_item
+        DROP COLUMN type;
+
+
+CREATE TABLE tb_food_items_tag (
+    food_item_id BIGINT NOT NULL,
+    tag VARCHAR(50) NOT NULL,
+    FOREIGN KEY (food_item_id) REFERENCES tb_food_item(id) ON DELETE CASCADE,
+)
