6096f7577df27d316c3cce39770dbe379d7a6531
Otávio Ximarelli <146153429+OtavioXimarelli@users.noreply.github.com>
2025-04-28 13:38:36 +0000

    feat: add missing RequestMapping annotation to RecipeController

--- src/main/java/com/otavio/aifoodapp/controller/RecipeController.java
+++ src/main/java/com/otavio/aifoodapp/controller/RecipeController.java
@@ -7,12 +7,14 @@ import com.otavio.aifoodapp.service.ChatService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/recipes")
 public class RecipeController {
 
     private final FoodItemService foodItemService;
