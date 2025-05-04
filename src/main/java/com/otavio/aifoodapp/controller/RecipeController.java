2241572d4cddbe55ec3db61491fcb1d1ba4fd655
otavio <ximarellio@gmail.com>
2025-05-04 16:32:17 -0300

    feat: add RecipeService to RecipeController and update analyzeRecipe method to accept recipe ID

--- src/main/java/com/otavio/aifoodapp/controller/RecipeController.java
+++ src/main/java/com/otavio/aifoodapp/controller/RecipeController.java
@@ -1,17 +1,22 @@
 package com.otavio.aifoodapp.controller;
 
 
+
 import com.otavio.aifoodapp.model.FoodItem;
+
 import com.otavio.aifoodapp.service.FoodItemService;
 import com.otavio.aifoodapp.service.ChatService;
 
+import com.otavio.aifoodapp.service.RecipeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/recipes")
@@ -19,10 +24,12 @@ public class RecipeController {
 
     private final FoodItemService foodItemService;
     private final ChatService chatService;
+    private final RecipeService recipeService;
 
-    public RecipeController(FoodItemService foodItemService, ChatService chatService) {
+    public RecipeController(FoodItemService foodItemService, ChatService chatService, RecipeService recipeService) {
         this.foodItemService = foodItemService;
         this.chatService = chatService;
+        this.recipeService = recipeService;
     }
 
     @GetMapping("/gen")
@@ -33,10 +40,12 @@ public class RecipeController {
                 .defaultIfEmpty(ResponseEntity.notFound().build());
 
     }
-
-    @GetMapping("/analyze")
-    public Mono<ResponseEntity<Mono<String>>> analyzeRecipe() {
-
+    @GetMapping("/analyze{id}")
+    public Mono<ResponseEntity<String>> analyzeRecipe(@PathVariable Long id) {
+        return Mono.justOrEmpty(recipeService.findById(id))
+                .flatMap(chatService::analyzeNutritionalProfile)
+                        .map(ResponseEntity::ok)
+                        .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
 }
