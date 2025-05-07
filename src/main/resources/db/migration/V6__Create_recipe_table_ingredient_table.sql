3f0126edbf92cfd005a174dfd6900d0d93fa30c7
otavio <ximarellio@gmail.com>
2025-05-06 23:57:05 -0300

    feat: create tb_recipes table and update tb_recipe_ingredients table structure

--- src/main/resources/db/migration/V6__Create_recipe_table_ingredient_table.sql
+++ src/main/resources/db/migration/V6__Create_recipe_table_ingredient_table.sql
@@ -1,10 +1,14 @@
+CREATE TABLE tb_recipes (
+                            id BIGSERIAL PRIMARY KEY,
+                            name VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE tb_recipe_ingredients (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    recipe_id BIGINT,
-    unit TEXT,
-    quantity DOUBLE PRECISION,
-
-    FOREIGN KEY (recipe_id) REFERENCES tb_recipes(id),
-    FOREIGN KEY (food_item_id) REFERENCES tb_food_item(id)
-
+                                       id BIGSERIAL PRIMARY KEY,
+                                       recipe_id BIGINT NOT NULL,
+                                       food_item_id BIGINT NOT NULL,
+                                       unit TEXT,
+                                       quantity DOUBLE PRECISION,
+                                       CONSTRAINT fk_recipe_ingredients_recipe FOREIGN KEY (recipe_id) REFERENCES tb_recipes(id),
+                                       CONSTRAINT fk_recipe_ingredients_food_item FOREIGN KEY (food_item_id) REFERENCES tb_food_item(id)
 );
