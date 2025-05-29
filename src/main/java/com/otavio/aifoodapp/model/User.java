b585b3d12420d2897e46c2c4bd754d2946ca0d5b
Otávio Ximarelli <146153429+OtavioXimarelli@users.noreply.github.com>
2025-05-29 14:29:40 +0000

    feat: implement UserDetails interface in User class and update user role handling

--- src/main/java/com/otavio/aifoodapp/model/User.java
+++ src/main/java/com/otavio/aifoodapp/model/User.java
@@ -1,10 +1,23 @@
 package com.otavio.aifoodapp.model;
 
+import com.otavio.aifoodapp.enums.UserRoles;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-@Entity
+import java.util.List;
+
+@Entity(name = "users")
 @Table(name = "tb_users")
-public class User {
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User implements UserDetails {
 
 
     @Id
@@ -12,13 +25,29 @@ public class User {
     private Long id;
 
     @Column(unique = true, nullable = false)
-    private String username;
+    private String login;
 
     @Column(nullable = false)
     private String password;
     private String firstName;
     private String lastName;
     private String email;
+    private UserRoles role;
 
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public java.util.Collection<? extends org.springframework.security.core.GrantedAuthority> getAuthorities() {
+        if (this.role == UserRoles.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
 
 }
