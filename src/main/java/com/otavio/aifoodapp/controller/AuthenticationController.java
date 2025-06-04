7ff4ab79dfe794ce8bf01437433c1ddc5581d281
otavioINTELLIJ <ximarellio@gmail.com>
2025-06-04 17:04:27 -0300

    feat: add user registration endpoint to AuthenticationController

--- src/main/java/com/otavio/aifoodapp/controller/AuthenticationController.java
+++ src/main/java/com/otavio/aifoodapp/controller/AuthenticationController.java
@@ -1,10 +1,13 @@
 package com.otavio.aifoodapp.controller;
 
 import com.otavio.aifoodapp.dto.AuthenticationDTO;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.otavio.aifoodapp.dto.RegisterDTO;
+import com.otavio.aifoodapp.model.User;
+import com.otavio.aifoodapp.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +17,18 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
+
 public class AuthenticationController {
-    @Autowired
-    private AuthenticationManager authenticationManager;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final UserRepository userRepository;
+
+    public AuthenticationController(AuthenticationManager authenticationManager, UserRepository userRepository) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+    }
+
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
@@ -24,5 +36,17 @@ public class AuthenticationController {
         var auth = this.authenticationManager.authenticate(usernamePassword);
 
         return ResponseEntity.ok(auth);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO data) {
+        if (userRepository.findByLogin(data.login()).isPresent()) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+
+        User newUser = new User(data.login(), encryptedPassword, data.role());
+        userRepository.save(newUser);
+
+        return ResponseEntity.ok().build();
     }
 }
