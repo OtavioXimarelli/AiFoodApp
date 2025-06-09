fc1c33a6d35fabca0bd6b1efb2f4cb8271d0c5d5
otavioINTELLIJ <ximarellio@gmail.com>
2025-06-08 22:45:00 -0300

    fix: load environment variables using Dotenv in AiFoodAppApplication

--- src/main/java/com/otavio/aifoodapp/AiFoodAppApplication.java
+++ src/main/java/com/otavio/aifoodapp/AiFoodAppApplication.java
@@ -1,13 +1,16 @@
 package com.otavio.aifoodapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.ai.autoconfigure.openai.OpenAiAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication(exclude = { OpenAiAutoConfiguration.class })
+@SpringBootApplication(exclude = {OpenAiAutoConfiguration.class})
 public class AiFoodAppApplication {
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
         SpringApplication.run(AiFoodAppApplication.class, args);
     }
 }
