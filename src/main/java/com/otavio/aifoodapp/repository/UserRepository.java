0a1cb6db576deca6394d3afda04ea9ba436b8da4
Otávio Ximarelli <146153429+OtavioXimarelli@users.noreply.github.com>
2025-06-10 14:21:19 +0000

    refactor: remove unused Optional import in UserRepository

--- src/main/java/com/otavio/aifoodapp/repository/UserRepository.java
+++ src/main/java/com/otavio/aifoodapp/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.otavio.aifoodapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
