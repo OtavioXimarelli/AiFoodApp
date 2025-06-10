259fcb2ffb5ad04c244518cf3a105e43919f5159
Otávio Ximarelli <146153429+OtavioXimarelli@users.noreply.github.com>
2025-06-10 17:52:21 +0000

    refactor: change RegisterDTO from record to class for enhanced flexibility

--- src/main/java/com/otavio/aifoodapp/dto/RegisterDTO.java
+++ src/main/java/com/otavio/aifoodapp/dto/RegisterDTO.java
@@ -2,5 +2,27 @@ package com.otavio.aifoodapp.dto;
 
 import com.otavio.aifoodapp.enums.UserRoles;
 
-public record RegisterDTO(String login, String password, UserRoles role) {
+public class RegisterDTO {
+    private final String login;
+    private final String password;
+    private final UserRoles role;
+
+    public RegisterDTO(String login, String password, UserRoles role) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+    public String login() {return login;}
+    public String password() { return password;
 }
+    public UserRoles role() {return role;}
+
+    @Override
+    public String toString() {
+        return "RegisterDTO{" +
+                "login='" + login + '\'' +
+                ", role=" + role +
+                '}';
+    }
+}
+
