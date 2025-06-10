5f0be49dd21aa6daf3a47aae4d52aac0439055c7
Otávio Ximarelli <146153429+OtavioXimarelli@users.noreply.github.com>
2025-06-10 14:26:19 +0000

    refactor: remove dotenv configuration from main application class

--- src/main/java/com/otavio/aifoodapp/AiFoodAppApplication.java
+++ src/main/java/com/otavio/aifoodapp/AiFoodAppApplication.java
@@ -9,13 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(exclude = {OpenAiAutoConfiguration.class})
 public class AiFoodAppApplication {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-        // don't know if is the best way to do this, but for now is what works
-        System.setProperty("MARITACA_API_KEY", dotenv.get("MARITACA_API_KEY", ""));
-        System.setProperty("MARITACA_API_URL", dotenv.get("MARITACA_API_URL", ""));
-        System.setProperty("MARITACA_API_MODEL", dotenv.get("MARITACA_API_MODEL", ""));
-        System.setProperty("MARITACA_SYSTEM_PROMPT", dotenv.get("MARITACA_SYSTEM_PROMPT", ""));
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET", ""));
         SpringApplication.run(AiFoodAppApplication.class, args);
     }
 }
