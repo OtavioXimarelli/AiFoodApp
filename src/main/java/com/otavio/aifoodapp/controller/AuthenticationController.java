95784f82303f3994b91764d82aefa161d7d1b3f8
otavio <ximarellio@gmail.com>
2025-06-22 17:51:11 -0300

    feat: enhance login method to handle invalid credentials with custom exception

--- src/main/java/com/otavio/aifoodapp/controller/AuthenticationController.java
+++ src/main/java/com/otavio/aifoodapp/controller/AuthenticationController.java
@@ -3,11 +3,14 @@ package com.otavio.aifoodapp.controller;
 import com.otavio.aifoodapp.dto.AuthenticationDTO;
 import com.otavio.aifoodapp.dto.LoginResponseDTO;
 import com.otavio.aifoodapp.dto.RegisterDTO;
+import com.otavio.aifoodapp.dto.RegisterResponseDTO;
+import com.otavio.aifoodapp.exception.UsernameOrPasswordInvalidExcpetion;
 import com.otavio.aifoodapp.model.User;
 import com.otavio.aifoodapp.repository.UserRepository;
 import com.otavio.aifoodapp.security.TokenService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,27 +30,31 @@ public class AuthenticationController {
     private final UserRepository userRepository;
     private final TokenService tokenService;
     private final BCryptPasswordEncoder passwordEncoder;
+
     public AuthenticationController(AuthenticationManager authenticationManager, UserRepository userRepository, TokenService tokenService, BCryptPasswordEncoder passwordEncoder) {
         this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
         this.tokenService = tokenService;
         this.passwordEncoder = passwordEncoder;
     }
-    
 
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody @Valid AuthenticationDTO data) {
-        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-        var auth = this.authenticationManager.authenticate(usernamePassword);
-        var token = tokenService.generateToken((User) auth.getPrincipal());
+    public ResponseEntity<?> login(@Valid @RequestBody AuthenticationDTO data) {
 
+        try {
+            var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+            var auth = this.authenticationManager.authenticate(usernamePassword);
+            var token = tokenService.generateToken((User) auth.getPrincipal());
+            return ResponseEntity.ok(new LoginResponseDTO(token, data.login(), auth.getAuthorities().toString()));
 
-        return ResponseEntity.ok(new LoginResponseDTO(token, data.login(), auth.getAuthorities().toString()));
+        } catch (BadCredentialsException e) {
+            throw new UsernameOrPasswordInvalidExcpetion("Invalid username or password", e);
+        }
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody @Valid RegisterDTO data) {
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterDTO data) {
         if (userRepository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
 
         String encryptedPassword = this.passwordEncoder.encode(data.password());
@@ -55,6 +62,6 @@ public class AuthenticationController {
         User newUser = new User(data.login(), encryptedPassword, data.role());
         userRepository.save(newUser);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok("User created");
     }
 }
