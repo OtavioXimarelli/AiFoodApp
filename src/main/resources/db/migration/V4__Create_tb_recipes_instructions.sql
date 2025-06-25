7c3a39d7246f23b154326f9c3c285c223cbeddac
otavio <ximarellio@gmail.com>
2025-06-25 00:29:37 -0300

    feat: change instructions field type to TEXT for longer recipe instructions

--- src/main/resources/db/migration/V4__Create_tb_recipes_instructions.sql
+++ src/main/resources/db/migration/V4__Create_tb_recipes_instructions.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tb_recipes_instructions (
     recipe_id BIGINT NOT NULL ,
     step_order INT NOT NULL ,
-    instructions VARCHAR(255),
+    instructions TEXT NOT NULL ,
     PRIMARY KEY (recipe_id, step_order) ,
     FOREIGN KEY (recipe_id) REFERENCES tb_recipes(id)
 
