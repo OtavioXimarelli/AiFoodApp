c321762019378cdd471fc378d704b22ac29e7d8b
otavio <ximarellio@gmail.com>
2025-06-25 00:29:41 -0300

    feat: change nutritional_info field type to TEXT for longer nutritional details

--- src/main/resources/db/migration/V5__Create_recipe_nutritional_info_table.sql
+++ src/main/resources/db/migration/V5__Create_recipe_nutritional_info_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE tb_recipes_nutritional_info (
                                              id BIGSERIAL PRIMARY KEY,
                                              recipe_id BIGINT NOT NULL,
-                                             nutritional_info VARCHAR(255),
+                                             nutritional_info TEXT NOT NULL,
                                              FOREIGN KEY (recipe_id) REFERENCES tb_recipes(id)
 );
