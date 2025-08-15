7ff3819f0bd7ab2c595238a6b1a205fef0d024f5
otavio <ximarellio@gmail.com>
2025-08-15 15:48:27 -0300

    feat(security): add authentication manager bean

--- src/main/java/com/otavio/aifoodapp/security/SecurityConfig.java
+++ src/main/java/com/otavio/aifoodapp/security/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.otavio.aifoodapp.security;
 
 import jakarta.servlet.DispatcherType;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -27,7 +30,7 @@ public class SecurityConfig {
     private String frontEndUrl;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, @Qualifier("configurationSource") CorsConfigurationSource corsConfigurationSource) throws Exception {
         return http
                 .cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .csrf(csrf -> csrf
@@ -72,6 +75,11 @@ public class SecurityConfig {
 
         return source;
 
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
