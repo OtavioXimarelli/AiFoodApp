5992c85d3ecd821f2ad3ac9ce546d7b337e2f2c9
otavio <ximarellio@gmail.com>
2025-08-15 15:48:19 -0300

    feat(auth): add endpoint to retrieve current user information

--- src/main/java/com/otavio/aifoodapp/controller/AuthenticationController.java
+++ src/main/java/com/otavio/aifoodapp/controller/AuthenticationController.java
@@ -1,22 +1,21 @@
 package com.otavio.aifoodapp.controller;
 
-import com.otavio.aifoodapp.dto.AuthenticationDTO;
-import com.otavio.aifoodapp.dto.LoginResponseDTO;
-import com.otavio.aifoodapp.dto.RegisterDTO;
-import com.otavio.aifoodapp.exception.UserAlreadyExistsException;
-import com.otavio.aifoodapp.exception.UsernameOrPasswordInvalidExcpetion;
+import com.otavio.aifoodapp.dto.UserDTO;
 import com.otavio.aifoodapp.model.User;
 import com.otavio.aifoodapp.repository.UserRepository;
-import com.otavio.aifoodapp.security.TokenService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -28,40 +27,33 @@ public class AuthenticationController {
     private final AuthenticationManager authenticationManager;
 
     private final UserRepository userRepository;
-    private final TokenService tokenService;
-    private final PasswordEncoder passwordEncoder;
 
-    public AuthenticationController(AuthenticationManager authenticationManager, UserRepository userRepository, TokenService tokenService, PasswordEncoder passwordEncoder) {
+    public AuthenticationController(AuthenticationManager authenticationManager, UserRepository userRepository) {
         this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
-        this.tokenService = tokenService;
-        this.passwordEncoder = passwordEncoder;
+
     }
 
 
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@Valid @RequestBody AuthenticationDTO data) {
-
-        try {
-            var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-            var auth = this.authenticationManager.authenticate(usernamePassword);
-            var token = tokenService.generateToken((User) auth.getPrincipal());
-            return ResponseEntity.ok(new LoginResponseDTO(token, data.login(), auth.getAuthorities().toString()));
-
-        } catch (BadCredentialsException e) {
-            throw new UsernameOrPasswordInvalidExcpetion("Invalid username or password", e);
+   @GetMapping
+    public ResponseEntity<UserDTO> getCurrentUser(Authentication authentication) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.notFound().build();
         }
-    }
 
-    @PostMapping("/register")
-    public ResponseEntity<?> register(@Valid @RequestBody RegisterDTO data) {
-        if (userRepository.findByLogin(data.login()) != null) throw new UserAlreadyExistsException("User already exists");
+        if (authentication.getPrincipal() instanceof OAuth2User oAuth2User) {
+            String email = oAuth2User.getAttributes().toString();
+            String name = oAuth2User.getAttributes().toString();
 
-        String encryptedPassword = this.passwordEncoder.encode(data.password());
 
-        User newUser = new User(data.login(), encryptedPassword, data.role());
-        userRepository.save(newUser);
-
-        return ResponseEntity.ok("User created");
-    }
+            Optional<User> dbUser = userRepository.findByEmail(email);
+            if (dbUser.isPresent()) {
+                return ResponseEntity.ok(UserDTO.fromUser(dbUser.get()));
+            } else {
+                UserDTO userDTO = new UserDTO(null, email, name, "USER");
+                return ResponseEntity.ok(userDTO);
+            }
+        }
+        return ResponseEntity.status(401).build();
+   }
 }
