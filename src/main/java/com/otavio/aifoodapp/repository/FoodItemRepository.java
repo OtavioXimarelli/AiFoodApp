fd25562785acff663ea60a4dab3cd93576d4197a
otavio <ximarellio@gmail.com>
2025-08-18 15:47:08 -0300

    refactor(repository): reorganize imports and improve method declarations in FoodItemRepository

--- src/main/java/com/otavio/aifoodapp/repository/FoodItemRepository.java
+++ src/main/java/com/otavio/aifoodapp/repository/FoodItemRepository.java
@@ -1,10 +1,15 @@
 package com.otavio.aifoodapp.repository;
 
-import com.otavio.aifoodapp.model.FoodItem;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.otavio.aifoodapp.model.FoodItem;
+import com.otavio.aifoodapp.model.User;
 
 @Repository
 public interface FoodItemRepository extends JpaRepository<FoodItem, Long> {
+    List<FoodItem> findByUser(User user);
+    List<FoodItem> findByUserId(Long userId);
 }
