f73180d30b47618f88e95373f2037293f150c01e
otavio <ximarellio@gmail.com>
2025-08-18 15:46:48 -0300

    refactor(model): reorganize imports in FoodItem class for clarity

--- src/main/java/com/otavio/aifoodapp/model/FoodItem.java
+++ src/main/java/com/otavio/aifoodapp/model/FoodItem.java
@@ -1,14 +1,27 @@
 package com.otavio.aifoodapp.model;
 
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import java.util.List;
 
 import com.otavio.aifoodapp.enums.FoodGroup;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "tb_food_item")
 @Data
@@ -42,4 +55,8 @@ public class FoodItem {
     @CollectionTable(name = "tb_food_tems_tags", joinColumns = @JoinColumn(name = "food_item_id"))  
     @Column(name = "tag")
     private List<String> tags;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
