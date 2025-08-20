d62aabeb6ac2757ddb4ec415ee3b5c058108ed89
otavio <ximarellio@gmail.com>
2025-08-20 10:33:16 -0300

    refactor(cors): remove unused imports in CorsConfig.java

--- src/main/java/com/otavio/aifoodapp/config/CorsConfig.java
+++ src/main/java/com/otavio/aifoodapp/config/CorsConfig.java
@@ -1,15 +1,14 @@
 package com.otavio.aifoodapp.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
 public class CorsConfig {
